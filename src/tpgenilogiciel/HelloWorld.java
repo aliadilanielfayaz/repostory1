e56bf11cd2a0e6ca977f31f1d475f354363122db
aliadilanielfayaz <aliadilanielfayaz@yahoo.fr>
2019-11-14 09:25:19 +0000

    premier message

---
+++ src/tpgenilogiciel/HelloWorld.java
@@ -0,0 +1,19 @@
+package tpgenilogiciel;
+
+public class HelloWorld {
+	public String getMessage( String chaine)
+	{
+	    if(chaine == " ")
+	    
+	    return "Helloworld";
+	    else
+	    	return chaine;
+	}
+
+	public int sum(int a,int b)
+	{
+		return a+b;
+	}
+
+}
+
